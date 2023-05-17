ed47e4be4ef01fccf4eae48a2e0b9aa55b04d57f
Dhruv Chaurasia <76548079+DhruvChaurasia3418@users.noreply.github.com>
2023-05-17 05:55:13 +0530

    Update 10Intersection.pl

--- 10Intersection.pl
+++ 10Intersection.pl
@@ -10,7 +10,7 @@ clauses
     Intersection([], _, []).
     Intersection([H|T], L2, [H|T3]) :- member(L2, H) , ! , Intersection(T, L2, T3).
 
-    Intersection([H|T], L2, L) :- Intersection(T, L2, L).
+    Intersection([_|T], L2, L) :- Intersection(T, L2, L).
 
     member([], _) :- fail.
     member([X|_], X).
